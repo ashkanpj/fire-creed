79c5d25ccb9f60e912ee431ec8477a3f459cd9c8
ashkanpj <ashkanpj.ap33@gmail.com>
2016-01-31 19:37:09 +0330

    OOOO

--- plugins/creed.lua
+++ plugins/creed.lua
@@ -1,16 +1,16 @@
 do
 
 function run(msg, matches)
-  return " صاحب جمال , اعلی حضرت , سرور من آقای ارسلان معروف به Creed  با ایدی : @creed_is_dead"
+  return "سلام اشی ادمین کابوس هستم ادمین تله فایر برای خرید گروه به ایدی @ashi_admin1_kaboos مراجعه کنید"
   end
 return {
   description = "Says about Creed ", 
   usage = "!Creed or Creed : Return Information about Creed hehehe",
   patterns = {
-    "^[Aa](rsalan)$",
-    "^[Cc](reed)$",
-    "^[!/]([Cc]reed)$",
-    "^[!/]([Aa]rsalan)$",
+    "^[Aa](shi)$",
+    "^[Ff](ire)$",
+    "^[!/]([Ff]ire)$",
+    "^[!/]([Aa]shi)$",
   },
   run = run
 }
