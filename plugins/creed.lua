6e889658c994d861d2e007d007b8bd83353e6d69
ashkanpj <ashkanpj.ap33@gmail.com>
2016-02-04 15:52:25 +0330

    fire.lua

--- plugins/creed.lua
+++ plugins/creed.lua
@@ -4,8 +4,8 @@ function run(msg, matches)
   return "سلام اشی ادمین کابوس هستم ادمین تله فایر برای خرید گروه به ایدی @ashi_admin1_kaboos مراجعه کنید"
   end
 return {
-  description = "Says about Creed ", 
-  usage = "!Creed or Creed : Return Information about Creed hehehe",
+  description = "Says about fire ", 
+  usage = "!fire or fire : Return Information about fire hehehe",
   patterns = {
     "^[Aa](shi)$",
     "^[Ff](ire)$",
